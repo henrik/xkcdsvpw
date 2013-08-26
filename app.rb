a6819c33165b2ce4510dd571c30cc6bdcbed3eb4
Henrik N <henrik@nyh.se>
2013-08-26 23:51:26 +0200

    Some polish.

--- app.rb
+++ app.rb
@@ -1,7 +1,34 @@
+# encoding: utf-8
+
 require "rubygems"
 require "bundler"
 Bundler.require :default, (ENV['RACK_ENV'] || "development").to_sym
 
 get "/" do
-  "Hello at #{Time.now}"
+  %{
+    <head>
+      <title>Svenskt XKCD-lösenord (lösenfras)</title>
+      <style>
+        body {
+          text-align: center;
+          padding: 20px;
+        }
+        .suggestion {
+          font-family: monospace;
+          font-size: 25px;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>Ditt nya lösenord:</h1>
+      <p class="suggestion">#{words(4).join(" ")}</p>
+    </body>
+  }
+end
+
+helpers do
+  def words(count = 4)
+    file = File.read("words.txt")
+    file.lines.to_a.sample(count).map(&:strip)
+  end
 end
